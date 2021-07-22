7b74da852c1eb294561e716739f29395cc09f765
Koop <61279093+KoopintheHub@users.noreply.github.com>
2021-07-22 05:37:37 -0700

    Add files via upload

--- JS/theme.js
+++ JS/theme.js
@@ -1,12 +1,10 @@
-var currectThemeId = -1;
-
 function restoreToOriginalTheme() {
     document.getElementById('themePage').innerHTML = "body { background: linear-gradient(to bottom, rgba(255, 255, 255, 1) 5%, rgba(255, 167, 0, 1) 100%); } #dlgbox { background: radial-gradient(circle, rgb(250, 255, 201) 10%, rgb(255, 207, 119) 40%, rgb(255, 182, 46) 90%); border: 3px solid #52490b; } button { background: radial-gradient(circle, rgb(250, 255, 201) 30%, rgba(255, 167, 0, 1) 100%); border: 1px solid black; color: black; }.titleBtn { box-shadow: 3px 3px 8px black; -moz-box-shadow: 3px 3px 8px black; -webkit-box-shadow: 3px 3px 8px black; -khtml-box-shadow: 3px 3px 8px black; }button:hover { background: radial-gradient(circle, rgb(201, 255, 252) 30%, rgb(0, 217, 255) 100%); border: 1px solid blue; color: blue; }.titleBtn:hover { box-shadow: 3px 3px 8px cyan; -moz-box-shadow: 3px 3px 8px cyan; -webkit-box-shadow: 3px 3px 8px cyan; -khtml-box-shadow: 3px 3px 8px cyan; }.titleBtn:active { box-shadow: 0px 0px 0px rgb(90, 90, 90); -moz-box-shadow: 0px 0px 0px rgb(90, 90, 90); -webkit-box-shadow: 0px 0px 0px rgb(90, 90, 90); -khtml-box-shadow: 0px 0px 0px rgb(90, 90, 90); }button:active { background: radial-gradient(circle, rgb(255, 255, 255) 30%, rgb(182, 182, 182) 100%); border: 1px solid white; color: #5a5a5a; }";
-    saveThemeToCookies();
+    saveThemeToCookies(-1);
 }
 
 function loadThemeCookie() {
-    changeTheme(getCookie("theme"));
+    if (getCookie("theme") == -1) restoreToOriginalTheme(); else changeTheme(getCookie("theme"));
 }
 
 function changeTheme(themeid) {
